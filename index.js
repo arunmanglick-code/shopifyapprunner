85b6629af9044fe102c57885869e7fbc8ae2bfcb
arunmanglick-code <arunmanglick@gmail.com>
2023-03-05 18:05:10 -0500

    Push Code

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+
+const port = process.env.PORT || 3030;
+
+app.get('/', (req, res) => res.send('Welcome to Shopify Research App!'))
+
+app.listen(port, (err) => {
+    if (err) {
+      console.log('Error::', err);
+    }
+      console.log(`Onexlab app listening on port ${port}`);
+  });
