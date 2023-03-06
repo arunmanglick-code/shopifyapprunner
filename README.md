a23109f533d1861febcfc33121c45ea9635e54b9
arunmanglick-code <arunmanglick@gmail.com>
2023-03-05 21:02:12 -0500

    Added two more routes

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # shopifyapprunner
  shopifyapprunner
+
+ # Ref: https://github.com/oxlb/Docker-NodeJsHelloWorld
+
+ Client ID: 84f4fff4e36bd4677a4b48368290c5dca
+ Client Secret: 9b1fe1da63821ad91f4aceb7cb9d340fa
+
--- index.js
+++ index.js
@@ -5,6 +5,32 @@ const port = process.env.PORT || 3030;
 
 app.get('/', (req, res) => res.send('Welcome to Shopify Research App!'))
 
+app.get("/shopify/appInstall",(req,res)=>{
+  res.send("Shopify App Installation Step1 !");
+
+  // // res.sendFile(__dirname + "/public/index.html");
+  //  let employees = [{id:1,name:"Ram"},
+  //                  {id:2,name:"RK"},
+  //                  {id:3,name:"Manish"},
+  //                  {id:4,name:"Abhijeet"},
+  //                  {id:5,name:"Varun"}];
+                   
+  // res.sendFile("/index.html");
+});
+
+app.get("/shopify/recieveAuthCode",(req,res)=>{
+  res.send("Shopify App Receive Authcode Step2 !");
+
+  // // res.sendFile(__dirname + "/public/index.html");
+  //  let employees = [{id:1,name:"Ram"},
+  //                  {id:2,name:"RK"},
+  //                  {id:3,name:"Manish"},
+  //                  {id:4,name:"Abhijeet"},
+  //                  {id:5,name:"Varun"}];
+                   
+  // res.sendFile("/index.html");
+});
+
 app.listen(port, (err) => {
     if (err) {
       console.log('Error::', err);
